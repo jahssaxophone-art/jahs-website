3c96278a687cacf19d68a1b285ccd5f24b7f1f7c
jahssaxophone-art <jahs.saxophone@gmail.com>
2026-06-17 22:52:05 -0500

    Update products.js

--- products.js
+++ products.js
@@ -1,15 +1,26 @@
 
 const products = [
   {
-    name: "JAHS Alto Classic",
-    price: "$199",
-    description: "Boquilla profesional para saxofón alto.",
+    name: "Perfecto #7 - A7",
+    price: "$75,000.00",
+    description: "Diseñada para estudiantes y músicos en formación que buscan calidad, facilidad de emisión y un sonido equilibrado, un boquilla impresa en 3D tipo Resina disponible para Saxo Alto y Saxo Tenor, la mejor opción para quienes quieren emprender una aventura musical en el saxofón.",
     image: "https://via.placeholder.com/300x200?text=Producto+1"
   },
   {
-    name: "JAHS Tenor Pro",
-    price: "$249",
-    description: "Sonido potente y gran proyección.",
+    name: "Perfecto #7 - A7a",
+    price: "$105,000.00",
+    description: "Boquilla diseñada para estudiantes de nivel intermedio que buscan una calidad sonora más definida, con una proyección de sonido cálida y centrada, favoreciendo una rápida evolución técnica.",
     image: "https://via.placeholder.com/300x200?text=Producto+2"
-  }
-];
+  },
+  {
+    name: "Resonator #7 - A7b",
+    price: "135,000.00",
+    description: "Boquilla diseñada para aquellos ejecutantes que buscan un sonido centrado, con gran proyección y una respuesta equilibrada en todos los registros, ideal para Jazz, música contemporánea y versátil.",
+    image: "https://via.placeholder.com/300x200?text=Producto+3"
+  },
+  {
+    name: "Kicker #7 - A7c",
+    price: "175,000.00",
+    description: "Boquilla diseñada especialmente para ejecutantes exigentes, con un diseño ergonómico y una proyección de sonido más definida con la cual podrás desenvolverte en el Jazz, Pop, Clásico y Fusión.",
+    image: "https://via.placeholder.com/300x200?text=Producto+4"
+   ] 
